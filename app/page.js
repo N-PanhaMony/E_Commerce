8ffd2b0393457f48e3686ebcef9a8329062c71b4
N-PanhaMony <neangpanhamony001@gmail.com>
2025-12-23 21:36:25 +0700

    Resolve conflict

--- app/page.js
+++ app/page.js
@@ -2,37 +2,21 @@ import ImageBanner from "@/components/ImageBanner";
 import Products from "@/components/Products";
 
 export async function getProducts() {
-  const response = await fetch('http://localhost:3000/api/products');
-  const products = await response.json();
-  return products;
+  const response = await fetch('http://localhost:3000/api/products')
+  const products = await response.json()
+  return products
 }
 
 export default async function Home() {
-  const products = await getProducts();
 
-  let painting = null;
-  let stickers = [];
-
-  for (let product of products) {
-    // check if this is Stripe's list object
-    if (product.data) {
-      // flatten the data array
-      stickers.push(...product.data);
-      continue;
-    }
-
-    // static painting
-    if (product.name === 'Angkor Wat') {
-      painting = product;
-      continue;
-    }
-  }
+  const products = await getProducts()
+  console.log(products)
 
   return (
     <div>
-      <ImageBanner />
+      <ImageBanner/>
       <section>
-        <Products painting={painting} stickers={stickers} />
+        <Products/>
       </section>
     </div>
   );
