1ed44e3d678b36471664e793d11bc9c4d834c7ef
N-PanhaMony <neangpanhamony001@gmail.com>
2025-12-30 02:25:10 +0700

    chore: prepare app for production deployment on AWS Amplify

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,       // keep React strict mode
-  output: 'standalone',        // for Amplify or serverless deployment
 
   // For static images served from /public
   images: {
